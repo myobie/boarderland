955342b9c2324b0deec54cd7146900598a778784
Chad Fowler <chad@chadfowler.com>
2014-01-10 13:29:38 +0100

    If I were a beginner I'd refactor it to look like this

--- app/controllers/timeline_controller.rb
+++ app/controllers/timeline_controller.rb
@@ -11,11 +11,14 @@ class TimelineController < ApplicationController
     @tasks_with_due_dates = split_into_sprints(tasks.select(&:due_date).sort_by(&:due_date)) #sorry about all this
   end
 
-  def split_into_sprints(tasks)
+  def sprint_boundaries
     start_date = Date.parse("3/1/2014")
-    sprint_boundaries = 26.times.map do |n|
+    @sprint_boundaries ||= 26.times.map do |n|
       start_date + ((n + 1) * 14 ) #every 2 weeks
     end
+  end
+
+  def split_into_sprints(tasks)
     sprints = []
     sprint_boundaries.inject(tasks) do |unselected_tasks, date|
       for_this_sprint, unclaimed = unselected_tasks.partition{|t| Date.parse(t.due_date) <= date }
