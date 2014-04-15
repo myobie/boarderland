5ab7ca65b4674910e8b986dd9b5a42404867b90c
myobie <me@nathanherald.com>
2014-04-15 23:01:46 +0200

    Draft of a worker to sync a person's data down

---
+++ app/workers/sync_worker.rb
@@ -0,0 +1,59 @@
+class SyncWorker
+  attr_reader :service
+
+  def initialize(service)
+    @service = service
+  end
+
+  def call
+    get_lists
+    get_users
+  end
+
+  def get_lists
+    resp = service.get("v1/lists")
+    if resp.success?
+      resp.each do |json|
+        list = List.find_or_create_with_json(json)
+        get_tasks_for_list(list)
+        get_comments_for_list(list)
+      end
+    end
+  end
+
+  def get_users
+    resp = service.get("v1/users")
+    if resp.success?
+      resp.each do |json|
+        User.find_or_create_with_json(json)
+      end
+    end
+  end
+
+  def get_tasks_for_list(list)
+    resp = service.get("v1/tasks", list_id: list.wunderlist_id)
+    if resp.success?
+      resp.each do |json|
+        Task.find_or_create_with_json(json)
+      end
+    end
+  end
+
+  def get_comments_for_list(list)
+    resp = service.get("v1/comments", list_id: list.wunderlist_id)
+    if resp.success?
+      resp.each do |json|
+        Comment.find_or_create_with_json(json)
+      end
+    end
+  end
+
+  def get_comments_for_task(task)
+    resp = service.get("v1/comments", task_id: task.wunderlist_id)
+    if resp.success?
+      resp.each do |json|
+        Comment.find_or_create_with_json(json)
+      end
+    end
+  end
+end
