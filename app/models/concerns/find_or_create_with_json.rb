9f58c035525f9a54a57652a666ecbd0e7ccbe7d7
myobie <me@nathanherald.com>
2014-04-17 11:36:27 +0200

    Getting closer

--- app/models/concerns/find_or_create_with_json.rb
+++ app/models/concerns/find_or_create_with_json.rb
@@ -3,8 +3,8 @@ module Concerns::FindOrCreateWithJson
 
   module ClassMethods
     def find_or_create_with_json(json)
-      model = find_or_create_by(wunderlist_id: json["id"])
-      model.update(data: json)
+      model = find_or_create_by(wunderlist_id: json.id)
+      model.update(data: json.to_hash)
       model
     end
   end
