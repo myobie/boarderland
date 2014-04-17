d0811199ba0a0d5fb713b6eafe39dad20453587f
myobie <me@nathanherald.com>
2014-04-17 11:46:49 +0200

    Track that junks

--- app/controllers/wunderlist_controller.rb
+++ app/controllers/wunderlist_controller.rb
@@ -5,9 +5,15 @@ class WunderlistController < ApplicationController
 
   def callback
     code = params[:code]
+
     access_token = Wunderlist.access_token(code)
     session[:access_token] = access_token
-    Integration.find_or_create_by(access_token: access_token)
+
+    integration = Integration.find_or_create_by(access_token: access_token)
+    user_info = wunderlist.get("v1/user")
+    user = User.find_or_create_by_json(user_info)
+    integration.update(wunderlist_user_id: user.wunderlist_id)
+
     redirect_to root_path
   end
 end
